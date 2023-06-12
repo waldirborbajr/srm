be80d9d0256bc54cbb1f5d42154e8144dffcdd9f
Waldir BORBA JUNIOR <wborbajr@gmail.com>
2023-06-12 09:28:00 -0300

    feat: first step of compress file after execute safe remove

--- command/srmcommand.go
+++ command/srmcommand.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"compress/zlib"
 	"flag"
 	"fmt"
 	"io"
@@ -18,26 +19,21 @@ Options:
 
 func SafeRemoveCommand() *Command {
 	cmd := &Command{
-		flags: flag.NewFlagSet("delete", flag.ExitOnError),
+		flags: flag.NewFlagSet("srm", flag.ExitOnError),
 		Execute: func(cmd *Command, args []string) {
 			if len(args) == 0 {
 				errAndExit("file name or directory is required")
 			}
 			file_name := args[0]
 
-			var wd string
-			var full_path string
+			var srcFullPath string
 			var err error
 
-			if wd, err = os.Getwd(); err != nil {
+			if srcFullPath, err = os.Getwd(); err != nil {
 				errAndExit("Unable to determinate content fullpath")
 			}
 
-			if full_path, err = filepath.Abs(filepath.Join(wd)); err != nil {
-				errAndExit("Unable to determinate content fullpath")
-			}
-
-			source := "{" + strings.Replace(full_path, "/", "-", -1) + "}"
+			source := "{" + strings.Replace(srcFullPath, "/", "-", -1) + "}"
 
 			homeDir, err := os.UserHomeDir()
 			if err != nil {
@@ -45,34 +41,19 @@ func SafeRemoveCommand() *Command {
 			}
 
 			filePath := filepath.Join(homeDir, ".srm", source) + file_name
-			fmt.Println(filePath)
 
 			// 1st Copy file to safety folder
-			src, err := os.Open(file_name)
-			if err != nil {
-				fmt.Fprint(os.Stderr, "srm: unable to save file. [src]")
-				os.Exit(-1)
-			}
-			dst, err := os.Create(filePath)
-			if err != nil {
-				fmt.Fprint(os.Stderr, "srm: unable to save file. [dst]")
-				os.Exit(-1)
-			}
+			srmDoCopy(file_name, filePath)
 
-			defer dst.Close()
-			_, err = io.Copy(dst, src)
-			src.Close()
-			if err != nil {
-				fmt.Fprint(os.Stderr, "srm: unable to save file. [cpy]")
-				os.Exit(-1)
-			}
+			// 2nd Compress file on target
+			srmCompress(filePath)
 
-			// 2nd Remove file
-			if err := os.Remove(file_name); err != nil {
-				fmt.Fprint(os.Stderr, "srm: unable to save file. [rmv]")
-				fmt.Println(err.Error())
-				os.Exit(-1)
-			}
+			// 3rd Remove source file
+			srmRemove(file_name)
+
+			// 4th Remove target uncompressed file
+			srmRemove(filePath)
+
 			fmt.Printf("srm: '%s' was safety deleted\n", file_name)
 		},
 	}
@@ -82,4 +63,61 @@ func SafeRemoveCommand() *Command {
 	}
 
 	return cmd
+}
+
+func srmDoCopy(srcFileName string, tgtPath string) {
+	src, err := os.Open(srcFileName)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "srm: unable to save file. [src]")
+		os.Exit(-1)
+	}
+
+	dst, err := os.Create(tgtPath)
+	if err != nil {
+		fmt.Fprint(os.Stderr, "srm: unable to save file. [dst]")
+		os.Exit(-1)
+	}
+
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	src.Close()
+	if err != nil {
+		fmt.Fprint(os.Stderr, "srm: unable to save file. [cpy]")
+		os.Exit(-1)
+	}
+}
+
+func srmCompress(srmFileToCompress string) {
+	// Create a new file "example.zlib"
+	file, err := os.Create(srmFileToCompress + ".zlib")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	// Create a new zlib writer with the best compression level
+	writer, err := zlib.NewWriterLevel(file, zlib.BestCompression)
+	if err != nil {
+		panic(err)
+	}
+	defer writer.Close()
+
+	// Open the input file "example.txt"
+	inputFile, err := os.Open(srmFileToCompress)
+	if err != nil {
+		panic(err)
+	}
+	defer inputFile.Close()
+
+	// Copy the contents of the input file to the writer
+	io.Copy(writer, inputFile)
+}
+
+func srmRemove(srmRmFileName string) {
+	if err := os.Remove(srmRmFileName); err != nil {
+		fmt.Fprint(os.Stderr, "srm: unable to save file. [rmv]")
+		fmt.Println(err.Error())
+		os.Exit(-1)
+	}
 }
